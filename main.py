51b274c7bc6cdd880727667e2018b6d5e304cc40
IOANNIS IASON BOIDANIS <61283674+JasonSDMN2001@users.noreply.github.com>
2023-05-22 15:02:20 +0300

    spectogram for a window of a sound file

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import librosa.display
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+signal, sr = librosa.load("sample-1.wav", duration=17)
+S_full, phase = librosa.magphase(librosa.stft(signal))
+idx = slice(*librosa.time_to_frames([7, 13], sr=sr))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.figure(figsize=(12, 4))
+plt.imshow(librosa.amplitude_to_db(S_full[:, idx], ref=np.max),
+           origin='lower', aspect='auto', cmap='hot')
+plt.colorbar(format='%+2.0f dB')
+plt.xlabel('Time')
+plt.ylabel('Frequency')
+plt.title('Spectrogram')
+plt.show()
+
+S_filter = librosa.decompose.nn_filter(S_full,
+                                       aggregate=np.median,
+                                       metric='cosine',
+                                       width=int(librosa.time_to_frames(2, sr=sr)))
+
+# The output of the filter shouldn't be greater than the input
+# if we assume signals are additive.  Taking the pointwise minimium
+# with the input spectrum forces this.
+S_filter = np.minimum(S_full, S_filter)
+
+margin_i, margin_v = 2, 10
+power = 2
+
+mask_i = librosa.util.softmask(S_filter,
+                               margin_i * (S_full - S_filter),
+                               power=power)
+
+mask_v = librosa.util.softmask(S_full - S_filter,
+                               margin_v * S_filter,
+                               power=power)
+
+# Once we have the masks, simply multiply them with the input spectrum
+# to separate the components
+
+S_foreground = mask_v * S_full
+S_background = mask_i * S_full
+
+plt.figure(figsize=(12, 8))
+plt.subplot(3, 1, 1)
+librosa.display.specshow(librosa.amplitude_to_db(S_full[:, idx], ref=np.max),
+                         y_axis='log', sr=sr)
+plt.title('Full spectrum')
+plt.colorbar()
+
+plt.subplot(3, 1, 2)
+librosa.display.specshow(librosa.amplitude_to_db(S_background[:, idx], ref=np.max),
+                         y_axis='log', sr=sr)
+plt.title('Background')
+plt.colorbar()
+plt.subplot(3, 1, 3)
+librosa.display.specshow(librosa.amplitude_to_db(S_foreground[:, idx], ref=np.max),
+                         y_axis='log', x_axis='time', sr=sr)
+plt.title('Foreground')
+plt.colorbar()
+plt.tight_layout()
+plt.show()
