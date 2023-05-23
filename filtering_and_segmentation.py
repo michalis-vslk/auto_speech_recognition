0cd18cf5f6fac1cd2d5efddbad27c2b55f69e093
PatroklosGeorgiadis <61284594+PatroklosGeorgiadis@users.noreply.github.com>
2023-05-23 18:28:20 +0300

    new files + comments

---
+++ filtering_and_segmentation.py
@@ -0,0 +1,113 @@
+from __future__ import print_function
+import numpy as np
+import librosa
+import librosa.display
+from scipy.signal import find_peaks
+
+import ploting
+
+
+# The function used in order to filter the original audio file
+def filtering(signal, sr):
+    # ---Input--- #
+    # signal: The original audio signal
+    # sr: The signal rate
+    # ---Output--- #
+    # foreground_audio: The filtered audio signal
+
+    # Emphasizing the signal through pre-emphasis, in order to recover the foreground audio more accurately later on
+    signal_emphasized = librosa.effects.preemphasis(signal)
+    S_full, phase = librosa.magphase(librosa.stft(signal_emphasized))
+    idx = slice(*librosa.time_to_frames([0, 18], sr=sr))
+
+    # ploting.plot_original_spectrogram(S_full, idx)
+
+    S_filter = librosa.decompose.nn_filter(S_full,
+                                           aggregate=np.median,
+                                           metric='cosine',
+                                           width=int(librosa.time_to_frames(2, sr=sr)))
+
+    # The output of the filter shouldn't be greater than the input
+    # if we assume signals are additive.  Taking the pointwise minimium
+    # with the input spectrum forces this.
+    S_filter = np.minimum(S_full, S_filter)
+
+    margin_i, margin_v = 2, 10
+    power = 2
+
+    mask_i = librosa.util.softmask(S_filter,
+                                   margin_i * (S_full - S_filter),
+                                   power=power)
+
+    mask_v = librosa.util.softmask(S_full - S_filter,
+                                   margin_v * S_filter,
+                                   power=power)
+
+    # Once we have the masks, we simply multiply them with the input spectrum
+    # to separate the components
+
+    s_foreground = mask_v * S_full
+    s_background = mask_i * S_full
+
+    # ploting.plot_foreground_background_comparison(S_full, s_background, s_foreground, idx, sr)
+
+    foreground_audio = librosa.istft(s_foreground * phase)
+
+    return foreground_audio
+
+
+# Segmenting the audio signal into words
+def segmentation(signal, foreground_audio):
+    # ---Input--- #
+    # signal: The original audio signal
+    # foreground_audio: The filtered audio signal
+    # ---Output--- #
+    # word_matrix: The matrix containing each word's signal
+
+    endpoints = librosa.effects.split(foreground_audio)
+
+    words = []
+    for start, end in endpoints:
+        word = signal[start:end]
+        words.append(word)
+
+    # Saving the words in a matrix
+    word_matrix = []
+    for word in words:
+        if len(word) > 5000:
+            word_matrix.append(word)
+
+    return word_matrix
+
+
+# Calculating the fundamental frequency of the speaker through all the isolated words
+def fundamental_frequency_calculator(word_matrix, sr):
+    # ---Input--- #
+    # word_matrix: The matrix containing each word's signal
+    # sr: The signal rate
+    # ---Output--- #
+    # avg_fundamental_freq: The fundamental frequency of the speaker
+
+    avg_fundamental_freq = 0
+    for i in range(len(word_matrix)):
+        word_array = word_matrix[i]
+        # Calculating autocorrelation of the word segment
+        autocorrelation = librosa.autocorrelate(word_array)
+
+        peaks, _ = find_peaks(autocorrelation)
+        # Finding the second-highest peak (excluding the lag 0 peak)
+        if len(peaks) > 1:
+            period = peaks[np.argmax(autocorrelation[peaks[1:]])]
+            fundamental_frequency = sr / period
+
+            # If it's above average speech frequency, we use a cap of 500 Hz
+            if fundamental_frequency > 500:
+                fundamental_frequency = 500
+            print("Word {}: Fundamental Frequency = {} Hz".format(i + 1, fundamental_frequency))
+            avg_fundamental_freq += fundamental_frequency
+        else:
+            print("Word {}: Fundamental Frequency not found.".format(i + 1))
+
+    avg_fundamental_freq = avg_fundamental_freq / len(word_matrix)
+
+    return avg_fundamental_freq
--- main.py
+++ main.py
@@ -1,155 +1,26 @@
 from __future__ import print_function
 import numpy as np
-import matplotlib.pyplot as plt
 import librosa
-#import sounddevice as sd
-#import noisereduce as nr
+# import sounddevice as sd
+# import noisereduce as nr
 import librosa.display
-from scipy.signal import find_peaks
+import filtering_and_segmentation
 
-#Loading the audio file
+
+# Loading the audio file
 signal, sr = librosa.load("sample-1.wav")  # we change sr
 
-signal_emphasized = librosa.effects.preemphasis(signal)
-S_full, phase = librosa.magphase(librosa.stft(signal_emphasized))
-idx = slice(*librosa.time_to_frames([0, 18], sr=sr))
+# 1) Filtering the original signal, isolating each word and calculating the speaker's fundamental frequency
+# Filtering with the use of a foreground vs background classifier
+foreground_audio = filtering_and_segmentation.filtering(signal, sr)
+# Isolating each word through segmentation
+word_matrix = filtering_and_segmentation.segmentation(signal, foreground_audio)
+# Calculating the fundamental frequency of the speaker
+average_fundamental_frequency = filtering_and_segmentation.fundamental_frequency_calculator(word_matrix, sr)
+print("The fundamental frequency of the speaker is calculated at: " + str(average_fundamental_frequency) + " Hz")
 
-plt.figure(figsize=(12, 4))
-plt.imshow(librosa.amplitude_to_db(S_full[:, idx], ref=np.max),
-           origin='lower', aspect='auto')
-plt.colorbar(format='%+2.0f dB')
-plt.xlabel('Time')
-plt.ylabel('Frequency')
-plt.title('Spectrogram')
-#plt.show()
 
-S_filter = librosa.decompose.nn_filter(S_full,
-                                       aggregate=np.median,
-                                       metric='cosine',
-                                       width=int(librosa.time_to_frames(2, sr=sr)))
-
-# The output of the filter shouldn't be greater than the input
-# if we assume signals are additive.  Taking the pointwise minimium
-# with the input spectrum forces this.
-S_filter = np.minimum(S_full, S_filter)
-
-margin_i, margin_v = 2, 10
-power = 2
-
-mask_i = librosa.util.softmask(S_filter,
-                               margin_i * (S_full - S_filter),
-                               power=power)
-
-mask_v = librosa.util.softmask(S_full - S_filter,
-                               margin_v * S_filter,
-                               power=power)
-
-# Once we have the masks, simply multiply them with the input spectrum
-# to separate the components
-
-S_foreground = mask_v * S_full
-S_background = mask_i * S_full
-
-plt.figure(figsize=(12, 8))
-plt.subplot(3, 1, 1)
-librosa.display.specshow(librosa.amplitude_to_db(S_full[:, idx], ref=np.max),
-                         y_axis='log', sr=sr)
-plt.title('Full spectrum')
-plt.colorbar()
-
-plt.subplot(3, 1, 2)
-librosa.display.specshow(librosa.amplitude_to_db(S_background[:, idx], ref=np.max),
-                         y_axis='log', sr=sr)
-plt.title('Background')
-plt.colorbar()
-plt.subplot(3, 1, 3)
-librosa.display.specshow(librosa.amplitude_to_db(S_foreground[:, idx], ref=np.max),
-                         y_axis='log', x_axis='time', sr=sr)
-plt.title('Foreground')
-plt.colorbar()
-plt.tight_layout()
-#plt.show()
-
-foreground_audio = librosa.istft(S_foreground * phase)
-endpoints = librosa.effects.split(foreground_audio)
-
-# Segment the audio signal into words
-words = []
-for start, end in endpoints:
-    word = signal[start:end]
-    words.append(word)
-
-# Optionally, apply additional processing techniques to refine the word segments
-
-# Save the words in a matrix or suitable data structure
-word_matrix = []
-for word in words:
-    if len(word) > 5000:
-        word_matrix.append(word)
-
-# Access individual words in the matrix
-for word in word_matrix:
-    # Process each word as needed
-
-    pass
-
-avg_fundamental_freq = 0
-for i in range(len(word_matrix)):
-    word_array = word_matrix[i]
-    # Calculate autocorrelation of the word segment
-    autocorr = librosa.autocorrelate(word_array)
-
-    peaks, _ = find_peaks(autocorr)
-    # Find the second-highest peak (excluding the trivial lag 0 peak)
-    if len(peaks) > 1:
-        period = peaks[np.argmax(autocorr[peaks[1:]])]
-        fundamental_freq = sr / period
-
-        # If it's above average speech frequency, we use a cap of 500 Hz
-        if fundamental_freq > 500:
-            fundamental_freq=500
-        print("Word {}: Fundamental Frequency = {} Hz".format(i+1, fundamental_freq))
-        avg_fundamental_freq += fundamental_freq
-    else:
-        print("Word {}: Fundamental Frequency not found.".format(i+1))
-
-avg_fundamental_freq = avg_fundamental_freq / len(word_matrix)
-print(str(avg_fundamental_freq) + " Hz")
-'''
-words = []
-current_part = []
-threshold = 1e-7
-for sample in foreground_audio:
-    if abs(sample) > threshold:
-        current_part.append(sample)
-    else:
-        words.append(current_part)
-        current_part = []'''
-
-#print(len(word_matrix))
-'''
-fig, ax = plt.subplots(nrows=2, sharex="all", sharey="all", constrained_layout=True)
-
-ax[0].set(title="Original Audio Waveform Graph", xlabel="TXT_TIME", ylabel="TXT_AMPLITUDE")
-ax[1].set(title="Audio Waveform Graph", xlabel="TXT_TIME", ylabel="TXT_AMPLITUDE")
-
-# Apply grid
-ax[0].grid()
-ax[1].grid()
-
-librosa.display.waveshow(signal, sr=sr, ax=ax[0], label="TXT_ORIGINAL")
-librosa.display.waveshow(signal, sr=sr, ax=ax[1], label="TXT_ORIGINAL")
-librosa.display.waveshow(foreground_audio, sr=sr, ax=ax[1], label="TXT_FILTERED")
-
-# Set legend
-ax[1].legend()
-
-# Show plot
-plt.show()
-'''
-'''for word in word_matrix:
-    print(len(word))'''
-'''
+''' For testing audio
 for i in range(len(word_matrix)):
     sd.play(word_matrix[i], sr)
     sd.wait()'''
