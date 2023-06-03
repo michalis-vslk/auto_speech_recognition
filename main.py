afc4e19dfe19c2741435b899ca55678d00465a75
IOANNIS IASON BOIDANIS <61283674+JasonSDMN2001@users.noreply.github.com>
2023-06-03 17:20:10 +0300

    7th mlp ,and the end

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
-import numpy as np
-import librosa
 import sounddevice as sd
-# import noisereduce as nr
+
 import matplotlib.pyplot as plt
 import librosa.display
 import filtering_and_segmentation
@@ -21,58 +19,37 @@ average_fundamental_frequency = filtering_and_segmentation.fundamental_frequency
 print("The fundamental frequency of the speaker is calculated at: " + str(average_fundamental_frequency) + " Hz")
 # 3
 t_dataset_matrix = training_datasets.training()
-'''for i in range(len(word_matrix)):
-    sd.play(word_matrix[i], sr)
-    sd.wait()
-print(len(word_matrix),len(t_dataset_matrix))
-for i in range(len(t_dataset_matrix)):
-    sd.play(t_dataset_matrix[i], sr)
-    sd.wait()'''
+
 spec1,spec2,total_max_shape,num_train_data,total_max_shape2 = training_datasets.cost_calculator(word_matrix,t_dataset_matrix)
 
-predicted_classes = training_datasets.classify_with_mlp(spec1,spec2,total_max_shape,num_train_data,total_max_shape2)
+predicted_classes,max_accuracy,history = training_datasets.classify_with_mlp(spec1,spec2,total_max_shape,num_train_data,total_max_shape2)
 print(predicted_classes)
-'''
-#this is our way,sort of. because we changed to check the dp amplitude
+while True:
+    if max_accuracy >= 0.7:
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('Model Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend(['Train', 'Validation'])
+        plt.show()
 
+        # Plot training and validation accuracy
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.title('Model Accuracy')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.legend(['Train', 'Validation'])
+        plt.show()
 
-# Divide the spectrograms into smaller windows
-window_size = 16  # Adjust the window size as needed
-hop_length = 8   # Adjust the hop length as needed
-
-#spectrogram_1_windows = librosa.util.frame(spectrogram_1, frame_length=window_size, hop_length=hop_length)
-#spectrogram_2_windows = librosa.util.frame(spectrogram_2, frame_length=window_size, hop_length=hop_length)
-
-# we seperate the magnitude in small windows so that we can compare them
-spectrogram_1_windows = librosa.util.frame(spec1_mag, frame_length=window_size, hop_length=hop_length)
-spectrogram_2_windows = librosa.util.frame(spec2_mag, frame_length=window_size, hop_length=hop_length)
-
-print(spectrogram_1_windows.shape)
-print(spectrogram_2_windows.shape)
-# Calculate the cosine similarity for each window
-cosine_similarities = []
-for i in range(spectrogram_1_windows.shape[1]):
-    vector_1 = spectrogram_1_windows[:, i].flatten()
-    vector_2 = spectrogram_2_windows[:, i].flatten()
-
-    # Pad or truncate the vectors to match the size
-    if len(vector_2) > len(vector_1):
-        size_diff = vector_2.size - vector_1.size
-        vector_1 = np.concatenate((vector_1, np.zeros(size_diff)))
+        break
     else:
-        size_diff = vector_1.size - vector_2.size
-        vector_2 = np.concatenate((vector_2, np.zeros(size_diff)))
+        predicted_classes, max_accuracy,history = training_datasets.classify_with_mlp(spec1, spec2, total_max_shape,
+                                                                              num_train_data, total_max_shape2)
 
-    # Compute cosine similarity between the vectors
-    cos_similarity = np.dot(vector_1, vector_2) / (np.linalg.norm(vector_1) * np.linalg.norm(vector_2))
-    cosine_similarities.append(cos_similarity)
-
-# Aggregate the cosine similarities
-average_cos_similarity = np.mean(cosine_similarities)
-print("Average Cosine Similarity:", average_cos_similarity)'''
-'''
  #For testing audio
-for i in range(len(word_matrix)):
+'''for i in range(len(word_matrix)):
     sd.play(word_matrix[i], sr)
     sd.wait()'''
 
